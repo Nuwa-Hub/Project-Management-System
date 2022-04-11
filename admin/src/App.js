dbe7e53ce20dea6e5cceff48a4ecab11083fff9c
Nuwa-Hub <you@example.com>
2022-04-11 06:59:17 +0530

    change

---
+++ admin/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Topbar from './components/topbar/Topbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Topbar/>
+    </div>
+  );;
+
+}
+
+export default App
