ed9fa1822b52d039722c2c245fecf5a0c7beebfc
Nuwa-Hub <you@example.com>
2022-05-23 14:46:00 +0530

    topbar

--- mainadmin/src/components/topbar/Topbar.jsx
+++ mainadmin/src/components/topbar/Topbar.jsx
@@ -4,8 +4,13 @@ import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import LanguageIcon from '@mui/icons-material/Language';
 import SettingsIcon from '@mui/icons-material/Settings';
 import userdp from "../../images/user.png"
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Topbar() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.currentUser);
+
+
   return (
     <div className="topbar">
       <div className="topbarWrapper">
